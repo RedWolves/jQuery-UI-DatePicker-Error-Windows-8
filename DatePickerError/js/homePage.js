aed4961c58c5bbb4c7fd9c48a78390efbc477746
Ralph Whitbeck <ralph.whitbeck@gmail.com>
2011-09-14 23:00:59 -0400

    initial import of a reduced test case that shows the error with datepicker

---
+++ DatePickerError/js/homePage.js
@@ -0,0 +1,19 @@
+﻿(function () {
+    'use strict';
+
+    // Custom event raised after the fragment is appended to the DOM.
+    WinJS.Application.addEventListener('fragmentappended', function handler(e) {
+        if (e.location === '/html/homePage.html') { fragmentLoad(e.fragment, e.state); }
+    });
+
+    function fragmentLoad(elements, options) {
+        WinJS.UI.processAll(elements)
+            .then(function () {
+                $("input").datepicker();
+            });
+    }
+
+    WinJS.Namespace.define('homePage', {
+        fragmentLoad: fragmentLoad,
+    });
+})();
